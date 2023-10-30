3948b41a7bc20ffd9a22a14be984acc4c4c6b93c
famecrush <101875033+famecrush@users.noreply.github.com>
2023-10-30 15:41:51 +0530

    Add files via upload

---
+++ ecommerce_scraper/spiders/products.py
@@ -0,0 +1,26 @@
+import scrapy
+from ecommerce_scraper.items import ProductItem
+
+class ProductsSpider(scrapy.Spider):
+    name = "products"
+    start_urls = ["https://books.toscrape.com/catalogue/category/books/psychology_26/index.html"]
+
+    def parse(self, response):
+        product_links = response.css("h3 a::attr(href)").getall()
+
+        for product_link in product_links:
+            yield response.follow(product_link, self.parse_product)
+
+        # Follow pagination links
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page:
+            yield response.follow(next_page, self.parse)
+
+    def parse_product(self, response):
+        item = ProductItem()
+        item["title"] = response.css("h1::text").get()
+        item["price"] = response.css("p.price_color::text").get()
+        item["description"] = response.css("meta[name='description']::attr(content)").get()
+        yield item
+
+
